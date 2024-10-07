f177ee6c1ea5055ad2f2263d42a88274330742f7
이정금 <jnggmi@pukyong.ac.kr>
2024-10-07 20:38:17 +0900

    잘못업로드했었음

---
+++ Assets/ljk/Script/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject Start_Btn;
+    public GameObject Load_Btn;
+    public GameObject Exit_Btn;
+    //public GameObject mainMenu; 옵션만들면 이거 SetActive(false)하면서
+    //public GameObject optionMenu;; 옵션메뉴 SetActive(true)하면 됨
+
+    // Start is called before the first frame update
+    public void Start_Btn_click()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit_Btn_click()
+    {
+        Application.Quit();
+    }
+
+
+
+
+}
