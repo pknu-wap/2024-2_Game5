27fecd6c624fd6af78a1d0bd0ab8bc491b8b5867
pipiPAPICO <163659664+pipiPAPICO@users.noreply.github.com>
2024-11-17 16:34:18 +0900

    knight animation
    스크립트 Boss 사용
    
    스프라이트 편집 필요
    
    보스 피가 50아래로 내려가면 스킬 사용(스킬 스프라이트 수정 필요)

--- Assets/lss/Scripts/player.cs
+++ Assets/lss/Scripts/player.cs
@@ -2,34 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public class Player : MonoBehaviour
+    public float playerHP = 100f;  // 플레이어의 HP
+
+    // 플레이어의 HP를 깎는 함수
+    public void TakeDamage(float damage)
     {
-        public float playerHP = 100f;  // 플레이어의 HP
+        playerHP -= damage;
+        Debug.Log("Player HP: " + playerHP);
 
-        // 플레이어의 HP를 깎는 함수
-        public void TakeDamage(float damage)
+        if (playerHP <= 0) // HP가 0이 되면 게임 오버
         {
-            playerHP -= damage;
-            Debug.Log("Player HP: " + playerHP);
-
-            if (playerHP <= 0) // HP가 0이 되면 game over
-            {
-                Debug.Log("Player is dead!");
-            }
+            Debug.Log("Player is dead!");
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 }
