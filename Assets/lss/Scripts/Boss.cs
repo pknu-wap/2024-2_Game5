291a57207ebc6f3051590d98136f35af2a1336ba
pipiPAPICO <163659664+pipiPAPICO@users.noreply.github.com>
2024-11-27 00:03:09 +0900

    bossScript
    bossScript

--- Assets/lss/Scripts/Boss.cs
+++ Assets/lss/Scripts/Boss.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngineInternal;
 
 public class Boss : MonoBehaviour
 {
@@ -10,8 +11,8 @@ public class Boss : MonoBehaviour
     private int nextMove;                // 무작위 움직임 설정
     public float decisionTime = 2f;      // 행동 결정 주기
     public float bossHP;
-    public float attackRange = 7f;
-    public float distance;
+    public float attackRange = 3f;
+
     public bool isAttacking = false;
     private int attackType;
     private bool isDamaging = false;
@@ -22,6 +23,8 @@ public class Boss : MonoBehaviour
 
     private bool isFacingLeft = true;    // 적이 왼쪽을 바라보는지 여부
 
+    private Vector2 bossRay;
+
     void Start()
     {
         bossHP = 100f;
@@ -29,7 +32,6 @@ public class Boss : MonoBehaviour
         bossRigidBody = GetComponent<Rigidbody2D>();
         bossAnimator = GetComponent<Animator>();
         bossSpriteRenderer = GetComponent<SpriteRenderer>();
-
         // 행동 결정 반복
         InvokeRepeating("DecideNextAction", 0f, decisionTime);
     }
@@ -40,18 +42,15 @@ public class Boss : MonoBehaviour
 
         // 애니메이션 상태 업데이트
         bossAnimator.SetBool("isGround", true);  // 항상 지면에 있다고 가정
-        bossAnimator.SetBool("isWalk", currentSpeed == walkSpeed);  // 걷기 애니메이션 조건
-     
 
-        distance = Vector3.Distance(this.transform.position, player.position); // 플레이어와의 거리 계산
-
-        if (distance <= attackRange && !isAttacking)
+        if (!isAttacking && Ray())
         {
+            currentSpeed = 0f;
             StartCoroutine(DelayedAttack());
-            Debug.Log("Update " + isAttacking);
-
         }
-        if(bossHP == 0)
+        else
+            bossAnimator.SetBool("isWalk", currentSpeed == walkSpeed);  // 걷기 애니메이션 조건
+        if (bossHP == 0)
         {
             currentSpeed = 0f;
             bossAnimator.SetBool("isDeath", true);
@@ -59,6 +58,7 @@ public class Boss : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        Ray();
         // 이동
         bossRigidBody.velocity = new Vector2(currentSpeed * (isFacingLeft ? -1 : 1), bossRigidBody.velocity.y);
     }
@@ -71,57 +71,75 @@ public class Boss : MonoBehaviour
     }
     void OnCollisionStay2D(Collision2D collision)
     {
-
+        Debug.Log(collision.gameObject.name);
         if (collision == null) Debug.Log("collision is null");
 
-        if (collision.gameObject.CompareTag("Player") && !isDamaging) // 플레이어와의 충돌, 공격 중일 때
+        if (collision.gameObject.CompareTag("Player")) // 플레이어와의 충돌, 공격 중일 때
         {
-            Debug.Log("if entered");
-            Debug.Log("if문 들어옴 "+ isDamaging);
-
-            Debug.Log("in if  " + isDamaging);
+            Debug.Log("if문 들어옴 isDamaging : "+ isDamaging);
             Player player = collision.gameObject.GetComponent<Player>();
-            if (player != null && isDamaging == true)
+            if (player != null && isDamaging)
             {
-                player.TakeDamage(4f); // 플레이어에게 4 데미지 입힘
-                Debug.Log("Player HP -= 4");
+                if (attackType == 3)
+                {
+                    player.TakeDamage(10f);
+                    isDamaging = false;
+                }
+                else
+                {
+                    player.TakeDamage(4f); // 플레이어에게 4 데미지 입힘
+                    Debug.Log("Player HP -= 4");
+                    isDamaging = false;
+                }
             }
         }
-        isDamaging = false;
+    }
+    bool Ray()
+    {
+        if (isFacingLeft)
+            bossRay = Vector2.left;
+        else
+            bossRay = Vector2.right;
+
+        Debug.DrawRay(transform.position, bossRay * attackRange, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, bossRay, attackRange, LayerMask.GetMask("Player"));
+        bool check = hit.collider != null;
+        return check;
     }
     IEnumerator DelayedAttack() // 공격 코루틴(2초 딜레이 후 공격)
     {
-        currentSpeed = 0f;
         isAttacking = true;  // 공격 중 상태로 변경
         isDamaging = true;
         Attack();        // 무작위 공격
 
-        yield return new WaitForSeconds(2.4f);
-        isAttacking = false;
+        yield return new WaitForSeconds(0.01f);
+        float bossAnimationTime = bossAnimator.GetCurrentAnimatorStateInfo(0).length;
+        yield return new WaitForSeconds(bossAnimationTime);
+
         bossAnimator.SetBool("isAttack", false);
         bossAnimator.SetBool("isAttack2", false);
         bossAnimator.SetBool("isAttack3", false);
+        bossAnimator.SetBool("isSkill", false);
         // 공격 완료 후 다시 공격 가능 상태로 변경
+
+        isAttacking = false;
     }
     void Attack()
     {
-        Debug.Log(bossHP);
-
-        attackType = Random.Range(0, 4);
-        Debug.Log(attackType);
+        if(bossHP > 50)
+            attackType = Random.Range(0, 3);
+        else
+            attackType = Random.Range(0, 4);
         switch (attackType) // 무작위로 공격 애니메이션 선택
         {
             case 0:
                 bossAnimator.SetBool("isAttack", true);
-                Debug.Log("Basic Attack");
                 break;
             case 1:
                 bossAnimator.SetBool("isAttack2", true);
-                Debug.Log("Kick Attack");
                 break;
             case 2:
                 bossAnimator.SetBool("isAttack3", true);
-                Debug.Log("Uppercut Attack");
                 break;
             case 3:
                 bossAnimator.SetBool("isSkill", true);
